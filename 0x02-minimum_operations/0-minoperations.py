e01f85dc9f0c415252d8495e6a7d77d74266ed40
Montanah <mititi.isaac@gmail.com>
2023-08-08 22:04:45 +0000

    Minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ Minimum Operations """
 
+
 def minOperations(n):
-    """ 
+    """
     Calculates the fewest number of operations needed to result in
     exactly n H characters in the file.
     """
