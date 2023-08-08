1014b6d0c90ddd28eadd8db952006464fce0741c
Montanah <mititi.isaac@gmail.com>
2023-08-08 14:55:11 -0700

    Minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Minimum Operations """
+
+import math
+
+
+def minOperations(n):
+    """ 
+    Calculates the fewest number of operations needed to result in
+    exactly n H characters in the file.
+    """
+    if n <= 1:
+        return 0
+    if n % 2 == 0:
+        return minOperations(n / 2) + 2
+    else:
+        for i in range(3, int(math.sqrt(n) + 1), 2):
+            if n % i == 0:
+                return minOperations(n / i) + i
+        return n
