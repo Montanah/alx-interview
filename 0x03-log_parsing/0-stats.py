bc67ec6770021b51fe2a10b9c2d913ab606fab1a
Montanah <mititi.isaac@gmail.com>
2023-08-16 07:08:17 -0700

    Log Parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""a script that reads stdin line by line and computes metrics:"""
+
+import sys
+
+
+def print_stats(status_codes, file_size):
+    """Prints the stats"""
+    print("File size: {}".format(file_size))
+    for key, value in sorted(status_codes.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
+
+
+if __name__ == "__main__":
+    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                    "403": 0, "404": 0, "405": 0, "500": 0}
+    file_size = 0
+    counter = 0
+    try:
+        for line in sys.stdin:
+            counter += 1
+            data = line.split()
+            try:
+                file_size += int(data[-1])
+            except:
+                pass
+            try:
+                if data[-2] in status_codes:
+                    status_codes[data[-2]] += 1
+            except:
+                pass
+            if counter % 10 == 0:
+                print_stats(status_codes, file_size)
+        print_stats(status_codes, file_size)
+    except KeyboardInterrupt:
+        print_stats(status_codes, file_size)
+        raise
