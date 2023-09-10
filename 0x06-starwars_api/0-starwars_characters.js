ce652edf6c691b69540a7f1913fd46c7296ae10e
Montanah <mititi.isaac@gmail.com>
2023-09-10 10:38:39 -0700

    a script that prints all characters of a Star Wars movie

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,25 +3,57 @@
 // The first argument is the Movie ID - example: 3 = “Return of the Jedi”  
 // Display one character name by line in the same order of the list “characters” in the /films/ endpoint
 // You must use the Star wars API https://swapi-api.alx-tools.com/api/
-// You must use the module request
+// You must use the request module
 
 
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const characters = JSON.parse(body).characters;
-    for (let i = 0; i < characters.length; i++) {
-      request(characters[i], function (error, response, body) {
+async function getCharacterNames(movieId) {
+  const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+  const filmData = await new Promise((resolve, reject) => {
+    request(filmUrl, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body));
+      }
+    });
+  });
+
+  const characterUrls = filmData.characters;
+  const characterNames = [];
+
+  for (const characterUrl of characterUrls) {
+    const characterData = await new Promise((resolve, reject) => {
+      request(characterUrl, (error, response, body) => {
         if (error) {
-          console.log(error);
+          reject(error);
         } else {
-          console.log(JSON.parse(body).name);
+          resolve(JSON.parse(body));
         }
       });
-    }
+    });
+
+    characterNames.push(characterData.name);
   }
-});
+
+  return characterNames;
+}
+
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.error('Please provide a Movie ID as a command-line argument.');
+} else {
+  getCharacterNames(movieId)
+    .then(names => {
+      if (names.length === 0) {
+        console.log('No characters found for the given Movie ID.');
+      } else {
+        names.forEach(name => console.log(name));
+      }
+    })
+    .catch(error => {
+      console.error('An error occurred:', error);
+    });
+}
